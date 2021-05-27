6564462c40e6effd4d7059ba8f974e80c38bc868
abdelhakim-instadeep <a.benechehab@instadeep.com>
2021-05-27 16:35:14 +0200

    add control suite

--- dreamerv2/train.py
+++ dreamerv2/train.py
@@ -7,10 +7,11 @@ import sys
 import warnings
 
 try:
-  import rich.traceback
-  rich.traceback.install()
+    import rich.traceback
+
+    rich.traceback.install()
 except ImportError:
-  pass
+    pass
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 logging.getLogger().setLevel('ERROR')
@@ -27,14 +28,13 @@ import agent
 import elements
 import common
 
-
 configs = pathlib.Path(sys.argv[0]).parent / 'configs.yaml'
 configs = yaml.safe_load(configs.read_text())
 config = elements.Config(configs['defaults'])
 parsed, remaining = elements.FlagParser(configs=['defaults']).parse_known(
     exit_on_help=False)
 for name in parsed.configs:
-  config = config.update(configs[name])
+    config = config.update(configs[name])
 config = elements.FlagParser(config).parse(remaining)
 logdir = pathlib.Path(config.logdir).expanduser()
 config = config.update(
@@ -48,11 +48,12 @@ tf.config.experimental_run_functions_eagerly(not config.jit)
 message = 'No GPU found. To actually train on CPU remove this assert.'
 assert tf.config.experimental.list_physical_devices('GPU'), message
 for gpu in tf.config.experimental.list_physical_devices('GPU'):
-  tf.config.experimental.set_memory_growth(gpu, True)
+    tf.config.experimental.set_memory_growth(gpu, True)
 assert config.precision in (16, 32), config.precision
 if config.precision == 16:
-  from tensorflow.keras.mixed_precision import experimental as prec
-  prec.set_policy(prec.Policy('mixed_float16'))
+    from tensorflow.keras.mixed_precision import experimental as prec
+
+    prec.set_policy(prec.Policy('mixed_float16'))
 
 print('Logdir', logdir)
 train_replay = common.Replay(logdir / 'train_replay', config.replay_size)
@@ -68,36 +69,42 @@ metrics = collections.defaultdict(list)
 should_train = elements.Every(config.train_every)
 should_log = elements.Every(config.log_every)
 
+
 def make_env(mode):
-  suite, task = config.task.split('_', 1)
-  if suite == 'dmc':
-    env = common.DMC(task, config.action_repeat, config.image_size)
-    env = common.NormalizeAction(env)
-  elif suite == 'atari':
-    env = common.Atari(
-        task, config.action_repeat, config.image_size, config.grayscale,
-        life_done=False, sticky_actions=True, all_actions=True)
-    env = common.OneHotAction(env)
-  else:
-    raise NotImplementedError(suite)
-  env = common.TimeLimit(env, config.time_limit)
-  env = common.RewardObs(env)
-  env = common.ResetObs(env)
-  return env
+    suite, task = config.task.split('_', 1)
+    if suite == 'dmc':
+        env = common.DMC(task, config.action_repeat, config.image_size)
+        env = common.NormalizeAction(env)
+    elif suite == 'atari':
+        env = common.Atari(
+            task, config.action_repeat, config.image_size, config.grayscale,
+            life_done=False, sticky_actions=True, all_actions=True)
+        env = common.OneHotAction(env)
+    elif suite == 'control':
+        env = common.Control(task, config.action_repeat, config.image_size)
+        env = common.NormalizeAction(env)
+    else:
+        raise NotImplementedError(suite)
+    env = common.TimeLimit(env, config.time_limit)
+    env = common.RewardObs(env)
+    env = common.ResetObs(env)
+    return env
+
 
 def per_episode(ep, mode):
-  length = len(ep['reward']) - 1
-  score = float(ep['reward'].astype(np.float64).sum())
-  print(f'{mode.title()} episode has {length} steps and return {score:.1f}.')
-  replay_ = dict(train=train_replay, eval=eval_replay)[mode]
-  replay_.add(ep)
-  logger.scalar(f'{mode}_transitions', replay_.num_transitions)
-  logger.scalar(f'{mode}_return', score)
-  logger.scalar(f'{mode}_length', length)
-  logger.scalar(f'{mode}_eps', replay_.num_episodes)
-  if mode == 'eval' or config.train_gifs:
-    logger.video(f'{mode}_policy', ep['image'])
-  logger.write()
+    length = len(ep['reward']) - 1
+    score = float(ep['reward'].astype(np.float64).sum())
+    print(f'{mode.title()} episode has {length} steps and return {score:.1f}.')
+    replay_ = dict(train=train_replay, eval=eval_replay)[mode]
+    replay_.add(ep)
+    logger.scalar(f'{mode}_transitions', replay_.num_transitions)
+    logger.scalar(f'{mode}_return', score)
+    logger.scalar(f'{mode}_length', length)
+    logger.scalar(f'{mode}_eps', replay_.num_episodes)
+    if mode == 'eval' or config.train_gifs:
+        logger.video(f'{mode}_policy', ep['image'])
+    logger.write()
+
 
 print('Create envs.')
 train_envs = [make_env('train') for _ in range(config.num_envs)]
@@ -111,47 +118,50 @@ eval_driver.on_episode(lambda ep: per_episode(ep, mode='eval'))
 
 prefill = max(0, config.prefill - train_replay.total_steps)
 if prefill:
-  print(f'Prefill dataset ({prefill} steps).')
-  random_agent = common.RandomAgent(action_space, logprob=True)
-  train_driver(random_agent, steps=prefill, episodes=1)
-  eval_driver(random_agent, episodes=1)
-  train_driver.reset()
-  eval_driver.reset()
+    print(f'Prefill dataset ({prefill} steps).')
+    random_agent = common.RandomAgent(action_space, logprob=True)
+    train_driver(random_agent, steps=prefill, episodes=1)
+    eval_driver(random_agent, episodes=1)
+    train_driver.reset()
+    eval_driver.reset()
 
 print('Create agent.')
 train_dataset = iter(train_replay.dataset(**config.dataset))
 eval_dataset = iter(eval_replay.dataset(**config.dataset))
 agnt = agent.Agent(config, logger, action_space, step, train_dataset)
 if (logdir / 'variables.pkl').exists():
-  agnt.load(logdir / 'variables.pkl')
+    agnt.load(logdir / 'variables.pkl')
 else:
-  config.pretrain and print('Pretrain agent.')
-  for _ in range(config.pretrain):
-    agnt.train(next(train_dataset))
+    config.pretrain and print('Pretrain agent.')
+    for _ in range(config.pretrain):
+        agnt.train(next(train_dataset))
+
 
 def train_step(tran):
-  if should_train(step):
-    for _ in range(config.train_steps):
-      _, mets = agnt.train(next(train_dataset))
-      [metrics[key].append(value) for key, value in mets.items()]
-  if should_log(step):
-    for name, values in metrics.items():
-      logger.scalar(name, np.array(values, np.float64).mean())
-      metrics[name].clear()
-    logger.add(agnt.report(next(train_dataset)), prefix='train')
-    logger.write(fps=True)
+    if should_train(step):
+        for _ in range(config.train_steps):
+            _, mets = agnt.train(next(train_dataset))
+            [metrics[key].append(value) for key, value in mets.items()]
+    if should_log(step):
+        for name, values in metrics.items():
+            logger.scalar(name, np.array(values, np.float64).mean())
+            metrics[name].clear()
+        logger.add(agnt.report(next(train_dataset)), prefix='train')
+        logger.write(fps=True)
+
+
 train_driver.on_step(train_step)
 
 while step < config.steps:
-  logger.write()
-  print('Start evaluation.')
-  logger.add(agnt.report(next(eval_dataset)), prefix='eval')
-  eval_driver(functools.partial(agnt.policy, mode='eval'), episodes=1)
-  print('Start training.')
-  train_driver(agnt.policy, steps=config.eval_every)
-  agnt.save(logdir / 'variables.pkl')
+    logger.write()
+    print('Start evaluation.')
+    logger.add(agnt.report(next(eval_dataset)), prefix='eval')
+    eval_driver(functools.partial(agnt.policy, mode='eval'), episodes=1)
+    print('Start training.')
+    train_driver(agnt.policy, steps=config.eval_every)
+    agnt.save(logdir / 'variables.pkl')
 for env in train_envs + eval_envs:
-  try:
-    env.close()
-  except Exception:
-    pass
+    try:
+        env.close()
+    except Exception:
+        pass
