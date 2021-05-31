d17285023a6b1ea08cfe22667c2c2cc65e34ad1b
abdelhakim-instadeep <a.benechehab@instadeep.com>
2021-05-31 18:09:40 +0200

    comment on static_scan

--- common/other.py
+++ common/other.py
@@ -31,17 +31,23 @@ class RandomAgent:
 
 def static_scan(fn, inputs, start, reverse=False):
     last = start
+    # outputs take the same size as start's entries: stoch, deter, mean, std
     outputs = [[] for _ in tf.nest.flatten(start)]
     indices = range(tf.nest.flatten(inputs)[0].shape[0])
     if reverse:
         indices = reversed(indices)
     for index in indices:
         inp = tf.nest.map_structure(lambda x: x[index], inputs)
+        # apply the function fn (img_step "prior") on last (state) and inp (action)
+        # -> prior = {'stoch': stoch, 'deter': deter, **stats}
         last = fn(last, inp)
+
+        # flatten returns [stoch, deter, mean, std], append outputs sub-elements
         [o.append(l) for o, l in zip(outputs, tf.nest.flatten(last))]
     if reverse:
         outputs = [list(reversed(x)) for x in outputs]
     outputs = [tf.stack(x, 0) for x in outputs]
+    # "pack sequence as" returns back to original structure {'stoch': stoch, ...}
     return tf.nest.pack_sequence_as(start, outputs)
 
 
